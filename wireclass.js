ed24d929afd077ff74955dfa54c8fc637a08db67
jonah <falsefriend13@gmail.com>
2016-11-30 14:35:21 -0500

    Update wireclass.js

--- wireclass.js
+++ wireclass.js
@@ -1,9 +1,75 @@
-function setup() {
-createCanvas(640, 640);
-background(200);
+var m = new Mover(6);
+var n = new Mover(6);
 
+
+function setup() {
+  createCanvas(640,640);
+  strokeWeight(2);
+  frameRate(60);
+  for(var i = 1; i < m.length; i++) {
+    m[i] = new Mover();
+  }
+  
+  for(var i = 1; i < n.length; i++) {
+    n[i] = new Mover();
+  }
+  
 }
 
-function draw () {
+function draw() {
+  background(255);
+  
+  beginShape();
+  for(var i = 1; i < m.length; i++) {
+    m[i].update();
+    m[i].checkEdges();
+    m[i].display(0,0,255);
+  }
+endShape(CLOSE);
+  
+  beginShape();
+  for(var i = 1; i < n.length; i++) {    
+    n[i].update();
+    n[i].checkEdges();
+    n[i].display(0,255,0);    
+  }
+  endShape(CLOSE);
+
+function Mover {
+
+  Mover() {
+    position = createVector(width/2, height/2);
+    velocity = createVector(0, 0);
+    var topspeed = 10;
+  }
+
+  void update() {
+
+    acceleration = createVector.random2D();
+    acceleration.mult(random(2));
+
+    velocity.add(acceleration);
+    velocity.limit(topspeed);
+    position.add(velocity);
+  }
+
+  void display(var r, var g, var b) {
+    noStroke();
+    fill(r, g, b);
+    //ellipse(position.x, position.y, 10, 10);
+    //noFill();
+    stroke(r, g, b);
+    vertex(position.x, position.y);
+  }
+
+  void checkEdges() {
+    if ((position.x > width-100) || (position.x < 100)) {
+      velocity.x = velocity.x * -1;
+    }
+    if ((position.y > height-100) || (position.y < 100)) {
+      velocity.y = velocity.y * -1;
+    }
+  }
+}
 
 }
